7b31b2cd666dae2d0196b1a7cbc0bf5ecb494c29
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-07-23 17:53:05 +0300

    added launcScreen, appIcon, func reliations

--- XylopApp/ViewController.swift
+++ XylopApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,33 +15,63 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    var player: AVAudioPlayer?
+
+    func playSound(_ soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
     @IBAction func keyPressedC(_ sender: UIButton)
     {
-        
+      playSound("C")
     }
     @IBAction func keyPressedD(_ sender: UIButton)
     {
-        
+        playSound("D")
+
     }
     @IBAction func keyPressedE(_ sender: UIButton)
-    {
+    {      playSound("E")
+
         
     }
     @IBAction func keyPressedF(_ sender: UIButton)
     {
-        
+        playSound("F")
+
     }
     @IBAction func keyPressedG(_ sender: UIButton)
     {
-        
+        playSound("G")
+
     }
     @IBAction func keyPressedA(_ sender: UIButton)
     {
-        
+        playSound("A")
+
     }
     @IBAction func keyPressedB(_ sender: UIButton)
     {
-        
+        playSound("B")
+
     }
 
 
